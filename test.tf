c19f7d97c65271183958b5a4ff9f1cef0c912dc1
mastermole0310 <mastermole@yandex.ru>
2021-12-13 22:33:02 +0200

    first commit

---
+++ test.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+
+resource "aws_instance" "mywebserver" {
+  ami           = "ami-0d718c3d715cec4a7"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.mywebserver.id}"]
+  key_name               = "alex"
+  user_data = <<EOF
+  #!/bin/bash
+  yum update
+  amazon-linux-extras install docker
+  chkconfig docker on
+  EOF
+
+  tags = {
+    Name = "pizda"
+  }
+}
+resource "aws_security_group" "mywebserver" {
+  name        = "webserver security group"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3378
+    to_port     = 3378
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
