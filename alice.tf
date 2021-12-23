b41bdb991d8af1a8617534b440c3d22b83feca73
mastermole0310 <mastermole@yandex.ru>
2021-12-23 20:00:40 +0200

    best

---
+++ alice.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+
+resource "aws_instance" "mywebserver" {
+  ami                    = "ami-0d718c3d715cec4a7"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mywebserver.id]
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/tmp/docker-compose.yml"
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      host = self.public_ip
+      private_key = file("C:/Users/алексей/Desktop/alex.pem")
+  }
+  }
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+groupadd docker
+usermod -aG docker ec2-user
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose -f /tmp/docker-compose.yml up
+EOF
+
+
+  key_name = "alex"
+  tags = {
+    Name = "pizda"
+  }
+}
+
+resource "aws_security_group" "mywebserver" {
+  name        = "webserver security group"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
