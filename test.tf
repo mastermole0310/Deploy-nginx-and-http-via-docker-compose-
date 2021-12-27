d20feb35c73d47e7d234d6d52ba368e98567de2f
mastermole0310 <mastermole@yandex.ru>
2021-12-27 21:03:30 +0200

    final

--- test.tf
+++ test.tf
@@ -18,7 +18,7 @@ provider "aws" {
 resource "aws_instance" "mywebserver" {
   ami                    = "ami-0d718c3d715cec4a7"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.mywebserver.id]
+  vpc_security_group_ids = ["${aws_security_group.mywebserver.id}"]
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/tmp/docker-compose.yml"
@@ -27,20 +27,9 @@ resource "aws_instance" "mywebserver" {
       user     = "ec2-user"
       host = self.public_ip
       private_key = file("C:/Users/алексей/Desktop/alex.pem")
-  }
-  }
-  user_data = <<EOF
-#!/bin/bash
-yum update -y
-amazon-linux-extras install docker -y
-systemctl start docker
-groupadd docker
-usermod -aG docker ec2-user
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose -f /tmp/docker-compose.yml up
-EOF
+  } 
+}
+  user_data = file("userdata.sh")
 
 
   key_name = "alex"
